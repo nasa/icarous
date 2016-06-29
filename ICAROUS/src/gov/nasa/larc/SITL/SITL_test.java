eb5476516392a24280856adabcf829a3a7167cf7
swee <swee@umich.edu>
2016-06-29 17:16:42 -0400

    Added copyright to the header

--- ICAROUS/src/gov/nasa/larc/SITL/SITL_test.java
+++ ICAROUS/src/gov/nasa/larc/SITL/SITL_test.java
@@ -1,3 +1,13 @@
+/**
+ * ICAROUS Interface
+ * Contact: Swee Balachandran (swee.balachandran@nianet.org)
+ * 
+ * 
+ * Copyright (c) 2011-2016 United States Government as represented by
+ * the National Aeronautics and Space Administration.  No copyright
+ * is claimed in the United States under Title 17, U.S.Code. All Other
+ * Rights Reserved.
+ */
 import gov.nasa.larc.ICAROUS.*;
 
 public class SITL_test{
