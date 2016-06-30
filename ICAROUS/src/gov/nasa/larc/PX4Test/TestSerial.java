4ae0ca4575ca9c5e7677f51e88368895538000de
swee <swee@umich.edu>
2016-06-30 11:07:50 -0400

    Reorgainzed folder structure

--- ICAROUS/src/gov/nasa/larc/PX4Test/TestSerial.java
+++ ICAROUS/src/gov/nasa/larc/PX4Test/TestSerial.java
@@ -4,9 +4,11 @@ public class TestSerial{
     public static void main(String args[]){
 	AircraftData SharedData    = new AircraftData(AircraftData.INIT_MESSAGES);
 	ICAROUS_Interface Pixhawk  = new ICAROUS_Interface(args[0],SharedData);
+	FMS_Thread FMS             = new FMS_Thread("Flight management",SharedData,Pixhawk);
 	DAQ_Thread DAQ             = new DAQ_Thread("Data acquisition",SharedData,Pixhawk);
 	
 	DAQ.start();
+	FMS.start();
 
     }
 }
