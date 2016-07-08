c4f2cb2a43a126b87eed9c0162874d9e5fdb9783
swee <swee@umich.edu>
2016-07-08 15:49:08 -0400

    moved flight plan into aircraft data

--- Java/src/gov/nasa/larcfm/ICAROUS/AircraftData.java
+++ Java/src/gov/nasa/larcfm/ICAROUS/AircraftData.java
@@ -10,6 +10,7 @@
  */
 package gov.nasa.larcfm.ICAROUS;
 import java.util.*;
+import java.lang.*;
 
 public class AircraftData{
 
@@ -104,4 +105,161 @@ public class AircraftData{
 	aircraftPosition.UpdatePosition((float)lat,(float)lon,(float)alt_msl,(float)alt_agl);
     }
 
+}
+
+class Position{
+
+    float lat;
+    float lon;
+    float alt_msl;
+    float alt_agl;
+
+    public Position(){
+	lat   = 0.0f;
+	lon   = 0.0f;
+	alt_msl   = 0.0f;
+	alt_agl   = 0.0f;
+    }
+
+    public Position(float lat_in,float lon_in,float altmsl_in){
+	lat   = lat_in;
+	lon   = lon_in;
+	alt_msl   = altmsl_in;
+    }
+
+    public Position(float lat_in,float lon_in,float altmsl_in,float altagl_in){
+	lat     = lat_in;
+	lon     = lon_in;
+	alt_msl   = altmsl_in;
+	alt_agl   = altagl_in;
+    }
+
+    public void UpdatePosition(float lat_in,float lon_in,float altmsl_in,float altagl_in){
+	lat   = lat_in;
+	lon   = lon_in;
+	alt_msl   = altmsl_in;
+	alt_agl   = altagl_in;
+    }
+
+}
+
+class Waypoint extends Position{
+
+    int id;
+    float heading;
+
+    public Waypoint(){
+	super();
+	id        =0;
+	heading   =0.0f;
+    }
+
+    public Waypoint(int id_in,float lat_in,float lon_in,float altmsl_in,
+		    float heading_in){
+	super(lat_in,lon_in,altmsl_in);
+	id        = id_in;
+	heading   = heading_in;
+    }
+
+    public Waypoint(Waypoint wp){
+	super(wp.lat,wp.lon,wp.alt_msl,wp.alt_agl);
+	id = wp.id;
+	heading = wp.heading; 
+
+    }
+        
+}
+
+class Obstacle extends Position{
+
+    int id;
+
+    public Obstacle(int id_in,float lat_in, float lon_in, float altmsl_in){
+	super(lat_in,lon_in,altmsl_in);
+	id = id_in;
+    }
+    
+}
+
+class FlightPlan{
+
+    public List<Waypoint> wayPoints;
+    public int numWayPoints;
+    public float maxHorDev;
+    public float maxVerDev;
+    public float standOffDist;
+    public int nextWaypoint;
+    public Iterator wpIt = null;
+
+    public FlightPlan(){
+	wayPoints    = new ArrayList<Waypoint>();
+    }
+    
+    public void FlightPlanInfo(int num,float HorDev, float VerDev, float standDist){
+
+	numWayPoints = num;
+	maxHorDev    = HorDev;
+	maxVerDev    = VerDev;
+	standOffDist = standDist;
+    }
+
+    public void AddWaypoints(int index,Waypoint wp){
+	wayPoints.add(index,wp);
+
+	if(wpIt != null){
+	    wpIt = wayPoints.iterator();
+	}
+    }
+
+    public Waypoint GetWaypoint(int index){
+	return (Waypoint) wayPoints.get(index);
+    }
+
+    public void Copy(FlightPlan newFP){
+
+	numWayPoints = newFP.numWayPoints;
+	maxHorDev    = newFP.maxHorDev;
+	maxVerDev    = newFP.maxVerDev;
+	standOffDist = newFP.standOffDist;
+
+	for(int i=0;i<numWayPoints;i++){
+	    Waypoint wp = new Waypoint(newFP.GetWaypoint(i));
+	    this.AddWaypoints(i,wp);
+	}
+    }
+
+    public double[] Distance2Waypoint(Position currentPos,Waypoint wp){
+
+	double Dist[] = new double[2];
+	
+	double lat1 = currentPos.lat;
+	double lon1 = currentPos.lon;
+
+	double lat2 = wp.lat;
+	double lon2 = wp.lon;
+
+	double d2r  = Math.PI/180;
+	double r2d  = 180/Math.PI;
+	
+	double R    = 6371.0f;                                                           
+	double dLat = (lat2-lat1)*d2r;
+	double dLon = (lon2-lon1)*d2r;
+	lat1 = lat1*d2r;
+	lat2 = lat2*d2r;
+	
+	double a    = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+	double c    = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+	double D    = R * c;
+	
+	double y = Math.sin(dLon) * Math.cos(lat2);
+	double x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+	double H = Math.atan2(y, x)*r2d;
+
+	Dist[0] = D;
+	Dist[1] = H;
+
+	return Dist;
+	
+    }
+    
 }
