60b18f844a5e892c046d84dab56420ac8b807c35
swee <swee@umich.edu>
2016-07-15 16:22:25 -0400

    Added methods to Conflict class

--- Java/src/gov/nasa/larcfm/ICAROUS/Conflict.java
+++ Java/src/gov/nasa/larcfm/ICAROUS/Conflict.java
@@ -10,12 +10,99 @@
  */
 package gov.nasa.larcfm.ICAROUS;
 
-public class Conflict{
+import java.util.*;
+import java.lang.*;
 
-    public enum CF_TYPE{
-	NONE,KEEPIN, KEEPOUT, TRAFFIC, OBSTACLE, EXAMINE, FLIGHTPLAN
+enum CONFLICT_TYPE{
+	NONE,KEEP_IN, KEEP_OUT, TRAFFIC, OBSTACLE, EXAMINE, FLIGHTPLAN
+}
+
+enum PRIORITY_LEVEL{
+    LOW, MEDIUM, HIGH;
+}
+
+public class Conflict{   
+
+    CONFLICT_TYPE conflictType;
+    PRIORITY_LEVEL priority;
+
+    GeoFence fence;
+    GenericObject object;
+
+    public Conflict(PRIORITY_LEVEL level, CONFLICT_TYPE ctype,GeoFence GF){
+	priority     = level;
+	conflictType = ctype;
+	fence        = GF;	
     }
 
-    CF_TYPE conflictType;
+    public Conflict(PRIORITY_LEVEL level, CONFLICT_TYPE ctype,GenericObject OB){
+	priority     = level;
+	conflictType = ctype;
+        object       = OB;
+    }
+
+    public void SetPriority(PRIORITY_LEVEL level){
+	priority = level;
+    }
+
+    public int IsEqual(Conflict conf){
+
+	int val = 0; // -1 - not equal, 0-equal, 1-equal/diff priority
+	if(conflictType == conf.conflictType){
+
+	    switch(conflictType){
+		
+	    case KEEP_IN:
+	    case KEEP_OUT:
+		if (fence.ID == conf.fence.ID){
+		    if(priority == conf.priority)
+			return 0;
+		    else
+			return 1;
+		}
+		else
+		    return -1;
+		
+	    case TRAFFIC:
+	    case OBSTACLE:		
+	    case EXAMINE:
+
+		if (object.id == conf.object.id){
+		    if(priority == conf.priority)
+			return 0;
+		    else
+			return 1;
+		}
+		else
+		    return -1;
+		
+	    
+	    
+	    } // switch
+   
+	} // if
+	return -1;
+    }
+
+	    
+    public static void AddConflictToList(List<Conflict> conflictList, Conflict conf){
+	for(int i=0;i<conflictList.size();i++){
+
+	    Conflict con = (Conflict) conflictList.get(i);
+
+	    int check = con.IsEqual(conf);
+
+	    if(check < 0)
+		conflictList.add(conf);
+	    else if(check > 0)
+		con.SetPriority(conf.priority);
+	
+	}
+	
+    }
+
+    public static void RemoveConflicts(){
+
+    }
 
 }
