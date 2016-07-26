b542152d2d840dab5ce74919fff88454d0a1fa08
swee <swee@umich.edu>
2016-07-26 17:19:19 -0400

    added PX4 commands to run script

--- Java/run.sh
+++ Java/run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 if [ "$1" == 'SITL' ];then
-   echo "Launching SITL"
+   echo "Launching ICAROUS for SITL"
    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATM.jar SITL_test 14551 14552
 fi;
 
 if [ "$1" == 'COMBOX' ];then
    echo "Launching COMBOX"
    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATM.jar ComBox ./params/ComInput.txt
+fi;
+
+if [ "$1" == 'PX4' ];then
+   echo "Launcing ICAROUS for Pixhawk"
+   java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATM.jar PX4Test /dev/ttyO1 14552
 fi;
 
 if [ "$1" == 'TestVoldemort' ];then
