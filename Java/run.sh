a0d941a51dee5fe506fac63cf67df41584ecff20
swee <swee@umich.edu>
2016-08-17 17:09:07 -0400

    changed port number for GS

--- Java/run.sh
+++ Java/run.sh
@@ -2,14 +2,14 @@
 
 if [ "$1" == 'SITL' ];then
    echo "Launching ICAROUS for SITL"
-   java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar launch -v --sitl 14551 --com localhost 14552 14550
+   java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar launch -v --sitl 14551 --com localhost 14552 14553
 
 elif [ "$1" == 'PX4' ];then
     echo "Launching ICAROUS for Pixhawk"
-    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar launch -v --px4 /dev/ttyO1 --com localhost 14552 14550 --bc 230.1.1.1 5555
+    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar launch -v --px4 /dev/ttyO1 --com localhost 14552 14553 --bc 230.1.1.1 5555
 elif [ "$1" == 'GS' ];then
     echo "Launching Ground station test"
-    mavproxy.py --master=127.0.0.1:14550 --map --load-module geofence
+    mavproxy.py --master=127.0.0.1:14553 --map --load-module geofence
 elif [ "$1" == 'GPSdebug' ];then
    echo "GPS debugger"
    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar DebugGPS --px4 /dev/ttyO1
