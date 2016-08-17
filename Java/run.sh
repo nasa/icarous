f4911852f57bdbc0a610946522fb3e593c423c70
swee <swee@umich.edu>
2016-08-17 17:00:39 -0400

    modified run scrip

--- Java/run.sh
+++ Java/run.sh
@@ -9,7 +9,7 @@ elif [ "$1" == 'PX4' ];then
     java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar launch -v --px4 /dev/ttyO1 --com localhost 14552 14550 --bc 230.1.1.1 5555
 elif [ "$1" == 'GS' ];then
     echo "Launching Ground station test"
-    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar GStest -v --sitl 14551 --com 14552 --bc 230.1.1.1 5555
+    mavproxy.py --master=127.0.0.1:14550 --map --load-module geofence
 elif [ "$1" == 'GPSdebug' ];then
    echo "GPS debugger"
    java -cp bin:lib/jssc-2.8.0.jar:lib/FormalATMj/FormalATM.jar DebugGPS --px4 /dev/ttyO1
