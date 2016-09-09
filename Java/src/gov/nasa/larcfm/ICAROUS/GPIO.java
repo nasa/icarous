c1612069d0b3e1a20a2847a4a893302a84c8b6cf
swee <swee@umich.edu>
2016-09-09 10:10:47 -0400

    added GPIO interface to ICAROUS

---
+++ Java/src/gov/nasa/larcfm/ICAROUS/GPIO.java
@@ -0,0 +1,17 @@
+/**
+ * General purpose input/output Interface
+ * Contact: Swee Balachandran (swee.balachandran@nianet.org)
+ * 
+ * 
+ * Copyright (c) 2011-2016 United States Government as represented by
+ * the National Aeronautics and Space Administration.  No copyright
+ * is claimed in the United States under Title 17, U.S.Code. All Other
+ * Rights Reserved.
+ */
+package gov.nasa.larcfm.ICAROUS;
+
+public interface GPIO{
+
+    public void ReadGPIOport(int);
+
+}
