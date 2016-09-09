f2fa0f149e3d47f13b4746fd8700fba7970324a1
swee <swee@umich.edu>
2016-09-09 13:49:47 -0400

    added radio API

---
+++ Java/src/gov/nasa/larcfm/radio.java
@@ -0,0 +1,16 @@
+import gov.nasa.larcfm.ICAROUS.*;
+
+public class radio{
+    public static void main(String args[]){
+
+	Interface SOCKET  = new Interface(Interface.SOCKET,"localhost",
+				      Integer.parseInt(args[0]),
+				      Integer.parseInt(args[1]),null);
+
+	Interface SERIAL  = new Interface(Interface.SERIAL,args[2],null);
+
+	while(true){
+	    Interface.PassThrough(SOCKET,SERIAL);
+	}
+    }    
+}
