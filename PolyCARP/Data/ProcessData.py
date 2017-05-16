6f050487d380d104cf60cb2179a3ef8c06302397
swee <swee@umich.edu>
2017-05-16 16:16:46 -0400

    Completed script to process Matt's geofences

--- PolyCARP/Data/ProcessData.py
+++ PolyCARP/Data/ProcessData.py
@@ -1,12 +1,78 @@
 import numpy as np
+from numpy import sin,cos
+import xml.etree.ElementTree as ET
 
-data = np.loadtxt('boxes2Towers.txt',dtype='string')
+def save_geofence(filename):
 
+    top = ET.Element('Geofence')
+
+    for fence in geofenceList:
+        #print "constructing"
+        child1   = ET.SubElement(top,'fence',id=str(fence['id']))
+        child1_1 = ET.SubElement(child1,'type')
+        child1_1.text = str(fence['type'])
+        child1_2 = ET.SubElement(child1,'num_vertices')
+        child1_2.text = str(fence['numV'])
+        child1_3 = ET.SubElement(child1,'floor')
+        child1_3.text = str(fence['floor'])
+        child1_4= ET.SubElement(child1,'roof')
+        child1_4.text = str(fence['roof'])
+        for vertex in range(len(fence['Vertices'])):
+            child1_5 = ET.SubElement(child1,'vertex',id=str(vertex))
+            child1_5_1 = ET.SubElement(child1_5,'lat')
+            child1_5_1.text = str(fence['Vertices'][vertex][0])
+            child1_5_2 = ET.SubElement(child1_5,'lon')
+            child1_5_2.text = str(fence['Vertices'][vertex][1])
+
+
+    #print "writing"
+    ET.ElementTree(top).write(filename)
+
+def MakeGeofence(id,type,numV,floor,roof,vertices):
+
+    Vertices = []
+    for i in range(4):
+        Vertices.append((vertices[i,0],vertices[i,1]))
+
+    Geofence = {'id': id, 'type': type, 'numV': numV, 'floor': floor,
+    'roof': roof, 'Vertices': Vertices}
+
+    geofenceList.append(Geofence)
+
+def ECEF2LLA(origin,vertices):
+
+    R = 6371000  # radius of earth
+    oLat = origin[0]*np.pi/180
+    oLon = origin[1]*np.pi/180
+    oAlt = 0
+
+    # convert given positions from geodetic coordinate frame to ECEF
+    oX = (R + oAlt) * cos(oLat) * cos(oLon)
+    oY = (R + oAlt) * cos(oLat) * sin(oLon)
+    oZ = (R + oAlt) * sin(oLat)
+
+    verticesLLA = np.zeros((4,2))
+
+    for i in range(4):
+        pX = oX + vertices[i,0]
+        pY = oY + vertices[i,1]
+
+        pLon = np.arctan2(pY,pX)
+        pLat = np.arccos(pX/(R*cos(pLon)))
+
+        pLon = pLon*180/np.pi
+        pLat = pLat*180/np.pi
+
+        verticesLLA[i,0] = pLat
+        verticesLLA[i,1] = pLon
+
+    return verticesLLA
+
+data = np.loadtxt('boxesKlondike.txt',dtype='string')
 numFences = data.shape[0]
 
-
-
 FenceList = []
+geofenceList = []
 
 for i in range(numFences):
     
@@ -43,9 +109,18 @@ for i in range(numFences):
     fenceO = np.zeros((8,3))
     fenceO[0:4,:] = fence1s
     fenceO[4:8,:] = fence2s
-        
 
     FenceList.append(fenceO)
-        
-            
-        
+
+    origin = [37.1021769,-76.3872069]
+
+    fenceCCW = np.zeros((4,3))
+    fenceCCW[0:2,:] = fence1s[0:2,:]
+    fenceCCW[2,:]   = fence1s[3,:]
+    fenceCCW[3, :] = fence1s[2, :]
+
+    verticesLLA = ECEF2LLA(origin,fenceCCW)
+
+    MakeGeofence(i,1,4,0,100,verticesLLA)
+
+save_geofence('gfdataset.xml')
