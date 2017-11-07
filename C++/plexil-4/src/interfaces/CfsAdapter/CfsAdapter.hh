a3c3ea10b6e7d3b55c87c13b45e080e80c89efbd
swee <swee@umich.edu>
2017-11-07 16:56:40 -0500

    built CfsAdapter for Plexil

---
+++ C++/plexil-4/src/interfaces/CfsAdapter/CfsAdapter.hh
@@ -0,0 +1,85 @@
+//
+// Created by Swee Balachandran on 11/6/17.
+//
+
+#ifndef CFSADAPTER_HH
+#define CFSADAPTER_HH
+
+#include "InterfaceAdapter.hh"
+#include "State.hh"
+#include "Command.hh"
+#include "cfs-data-format.hh"
+#include "ThreadSemaphore.hh"
+#include "ThreadMutex.hh"
+
+#include <queue>
+
+namespace PLEXIL {
+
+    class CfsAdapter : public InterfaceAdapter
+    {
+    public:
+        CfsAdapter (AdapterExecInterface&, pugi::xml_node const);
+
+        bool initialize();
+        bool start();
+        bool stop();
+        bool reset();
+        bool shutdown();
+
+        void lookupNow(State const &state, StateCacheEntry &cacheEntry);
+        void subscribe(const State& state);
+        void unsubscribe(const State& state);
+
+        void executeCommand(Command *cmd);
+
+        void invokeAbort(Command *cmd);
+
+        void HandleReturnValue(PlexilCommandMsg msg);
+
+        void GetCmdQueueMsg(PlexilCommandMsg& msg);
+        void GetLookUpQueueMsg(PlexilCommandMsg& msg);
+
+    private:
+        std::queue<PlexilCommandMsg*> cmdQueue;
+        std::queue<PlexilCommandMsg*> lookupQueue;
+
+        //* @brief Semaphore for return values from LookupNow
+        ThreadSemaphore m_lookupSem;
+
+        /**
+         * @brief Mutex used to hold the processing of incoming return values while commands
+         * are being sent and recorded.
+         */
+        ThreadMutex m_cmdMutex;
+
+        //* @brief Place to store result of current pending LookupNow request
+        Value m_pendingLookupResult;
+
+        //* @brief Place to store state of pending LookupNow
+        State m_pendingLookupState;
+
+        //* @brief Serial # of current pending LookupNow request, or 0
+        uint32_t m_pendingLookupSerial;
+
+
+        //* brief Cache of command serials and their corresponding ack and return value variables
+        typedef std::map<uint32_t, Command *> PendingCommandsMap;
+
+        //* @brief Cache of ack and return value variables for commands we sent
+        PendingCommandsMap m_pendingCommands;
+
+        //* @brief Serial # of current pending command
+        uint32_t m_pendingCommandSerial;
+
+
+    };
+
+    extern "C" {
+    void initCfsAdapter();
+    }
+
+} // namespace PLEXIL
+
+
+#endif //CFSADAPTER_H
---
+++ C++/plexil-4/src/interfaces/CfsAdapter/cfs-data-format.hh
@@ -0,0 +1,45 @@
+//
+// Created by Swee on 11/7/17.
+//
+
+#ifndef CFS_DATA_FORMAT_H
+#define CFS_DATA_FORMAT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#ifdef CFS
+#include "cfe.h"
+#define TLM_HDR_SIZE CFE_SB_TLM_HDR_SIZE
+#define CMD_HDR_SIZE CFE_SB_CMD_HDR_SIZE
+#else
+#define TLM_HDR_SIZE 1
+#define CMD_HDR_SIZE 1
+#endif
+
+
+// Icarous Command types
+typedef enum {
+    _LOOKUP_,
+    _COMMAND_,
+    _LOOKUP_RETURN_,
+    _COMMAND_RETURN_,
+} messageType_t;
+
+typedef struct{
+    uint8_t TlmHeader[TLM_HDR_SIZE];
+    messageType_t mType;
+    int id;
+    char name[50];
+    double argsD[4];
+    bool argsB[4];
+}PlexilCommandMsg;
+
+
+}
+
+
+#endif //CFS_DATA_FORMAT_H
