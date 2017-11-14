ea7eaca27cb198de725da7caa57911a41bd6f4ca
Marco A. Feliu <marco.feliu@nianet.org>
2017-11-14 11:42:07 -0500

    Encapsulate DDS forwarding in a ArduPilot derived interface

--- C++/include/DDS/ArduPilotInterfaceWithDDSForwarding.hpp
+++ C++/include/DDS/ArduPilotInterfaceWithDDSForwarding.hpp
@@ -1,4 +1,16 @@
 #ifndef ICAROUS_ARDUPILOTINTERFACEWITHDDSFORWARDING_HPP
 #define ICAROUS_ARDUPILOTINTERFACEWITHDDSFORWARDING_HPP
 
+#include "INTERFACE/MAVLinkInterface.h"
+#include "DDS/Common.hpp"
+
+class ArduPilotInterfaceWithDDSForwarding : public ArduPilotInterface_t {
+private:
+    dds::pub::DataWriter<MAVLink::GPSInfo> gpsInfoWriter;
+public:
+    ArduPilotInterfaceWithDDSForwarding(Icarous_t *ic);
+
+    void HandlePosition(const mavlink_message_t *message) override;
+};
+
 #endif
--- C++/src/DDS/ArduPilotInterfaceWithDDSForwarding.cpp
+++ C++/src/DDS/ArduPilotInterfaceWithDDSForwarding.cpp
@@ -1,1 +1,36 @@
 #include "DDS/ArduPilotInterfaceWithDDSForwarding.hpp"
+#include "Icarous.h"
+
+ArduPilotInterfaceWithDDSForwarding::ArduPilotInterfaceWithDDSForwarding(Icarous_t *ic) :
+        gpsInfoWriter(dds::core::null),
+        ArduPilotInterface_t(ic) {
+    gpsInfoWriter = DataWriterFactory<MAVLink::GPSInfo>();
+}
+
+void ArduPilotInterfaceWithDDSForwarding::HandlePosition(const mavlink_message_t *message) {
+    mavlink_global_position_int_t globalPositionInt;
+    mavlink_msg_global_position_int_decode(message, &globalPositionInt);
+
+    position_t position;
+    position.time_gps = (double) globalPositionInt.time_boot_ms / 1E3;
+    position.latitude = (double) globalPositionInt.lat / 1E7;
+    position.longitude = (double) globalPositionInt.lon / 1E7;
+    position.altitude_abs = (double) globalPositionInt.alt / 1E3;
+    position.altitude_rel = (double) globalPositionInt.relative_alt / 1E3;
+    position.vx = (double) globalPositionInt.vx / 100;
+    position.vy = (double) globalPositionInt.vy / 100;
+    position.vz = (double) globalPositionInt.vz / 100;
+
+    MAVLink::GPSInfo gpsMsg(
+            position.time_gps,
+            position.latitude,
+            position.longitude,
+            position.altitude_abs,
+            position.altitude_rel,
+            position.vx,
+            position.vy,
+            position.vz);
+    this->gpsInfoWriter << gpsMsg;
+
+    icarous->InputPosition(&position);
+}
