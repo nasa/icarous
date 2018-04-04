c1dd70ea3c0128a325dda90e3da603cdf039cca3
swee <swee.balachandran@nianet.org>
2018-04-03 21:25:55 -0400

    Added helper functions for service requests/responds

--- cFS/apps/APFunctionsLib/fsw/platform_inc/APFunctions.h
+++ cFS/apps/APFunctionsLib/fsw/platform_inc/APFunctions.h
@@ -9,10 +9,24 @@
 #include "msgids/msgids.h"
 #include "Icarous_msg.h"
 
-#define InitMsg(cmd)\
+#define InitCmdMsg(cmd)\
 ArgsCmd_t cmd; \
 CFE_SB_InitMsg(&cmd,AP_COMMANDS_MID,sizeof(ArgsCmd_t),TRUE);
 
+#define InitTrafficServiceMsg(msg)\
+service_t msg; \
+CFE_SB_InitMsg(&msg,SERVICE_TRAFFIC_MID,sizeof(service_t),TRUE);\
+msg.id = (rand() + rand());
+
+#define InitTrajectoryServiceMsg(msg)\
+service_t msg; \
+CFE_SB_InitMsg(&msg,SERVICE_TRAJECTORY_MID,sizeof(service_t),TRUE);\
+msg.id = (rand() + rand());
+
+#define InitGeofenceServiceMsg(msg)\
+service_t msg; \
+CFE_SB_InitMsg(&msg,SERVICE_GEOFENCE_MID,sizeof(service_t),TRUE);\
+msg.id = (rand() + rand());
 
 int32 APFunctionsLibInit(void);
 void SetMode(icarous_control_mode_t mode);
@@ -25,5 +39,39 @@ void SetGPSPosition(double lat,double lon,double alt);
 void SetVelocity(double Vn, double Ve, double Vu);
 void SetSpeed(float speed);
 
+// helper functions to request services from geofence app
+int ServiceFence_GetFenceViolation(double* position,double* velocity);
+int ServiceFence_GetWPFeasibility(double* positionA,double* positionB);
+int ServiceFence_GetRecoveryPosition(double* position);
+
+// helper function to request services from traffic app
+int ServiceTraffic_GetTrafficConflict(bool bands,double* position,double* velocity);
+int ServiceTraffic_GetSafe2Turn(double* position,double* velocity,double headingA,double headingB);
+
+// helper functions to request services from trajectory app
+int ServiceTrajectory_GetWaypoint(char* planID,int index);
+int ServiceTrajectory_GetTotalWaypoints(char* planID);
+int ServiceTrajectory_GetNewPath(char* planID,char* algorithm,double* positionA,double* velocity,double* positionB);
+int ServiceTrajectory_GetXTrackDeviation(char* planID,int leg,double* position);
+int ServiceTrajectory_GetDistance(double* positionA,double* positionB);
+int ServiceTrajectory_GetExitPoint(char* planID,double* position,int nextWP);
+int ServiceTrajectory_GetInterceptHeading(char* planID,int nextWP,double* position);
+int ServiceTrajectory_GetInterceptManeuver(char* planID,int nextWP,double* position);
+
+// helper functions to decode service response
+void ServiceFence_DecodeFenceViolation(service_t* msg,bool* output);
+void ServiceFence_DecodeWPFeasibility(service_t* msg,bool* output);
+void ServiceFence_DecodeRecoveryPosition(service_t* msg,double* output);
+
+void ServiceTraffic_DecodeTrafficConflict(service_t* msg,double* output);
+void ServiceTraffic_DecodeSafe2Turn(service_t* msg,bool* output);
+
+void ServiceTrajectory_DecodeWaypoint(service_t* msg,double* output);
+void ServiceTrajectory_DecodeTotalWaypoints(service_t* msg,int* output);
+void ServiceTrajectory_DecodeNewPath(service_t* msg,int* output);
+void ServiceTrajectory_DecodeXTrackDeviation(service_t* msg,double* output);
+void ServiceTrajectory_DecodeDistance(service_t* msg,double* output);
+void ServiceTrajectory_DecodeExitPoint(service_t* msg,double* output);
+void ServiceTrajectory_DecodeInterceptHeading(service_t* msg,double* output);
 
 #endif //APFUNCTIONS_H
--- cFS/apps/APFunctionsLib/fsw/src/APFunctions.c
+++ cFS/apps/APFunctionsLib/fsw/src/APFunctions.c
@@ -3,19 +3,22 @@
 //
 
 #include "APFunctions.h"
+#include <time.h>
+#include <stdlib.h>
+#include <Icarous_msg.h>
 
 int32 APFunctionsLibInit(void)
 {
 
     OS_printf ("APFunctions Lib Initialized.\n");
-
+    srand(time(0));
     return CFE_SUCCESS;
 
 }
 
 // Set Mode
 void SetMode(icarous_control_mode_t mode){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _SETMODE_;
     cmd.param1 = mode;
     SendSBMsg(cmd);
@@ -23,7 +26,7 @@ void SetMode(icarous_control_mode_t mode){
 
 // Arm Motors
 void ArmThrottles(uint8_t c){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _ARM_;
     cmd.param1 = c;
     SendSBMsg(cmd);
@@ -31,7 +34,7 @@ void ArmThrottles(uint8_t c){
 
 // Set Next WP
 void SetNextMissionItem(int nextWP){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _GOTOWP_;
     cmd.param1 = nextWP;
     SendSBMsg(cmd);
@@ -39,7 +42,7 @@ void SetNextMissionItem(int nextWP){
 
 // Takeoff
 void Takeoff(float alt){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _TAKEOFF_;
     cmd.param1 = alt;
     SendSBMsg(cmd);
@@ -47,7 +50,7 @@ void Takeoff(float alt){
 
 // Land
 void Land(){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _LAND_;
     cmd.param5 = 0;
     cmd.param6 = 0;
@@ -57,7 +60,7 @@ void Land(){
 
 // Set yaw
 void SetYaw(uint8_t relative,double heading){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _SETYAW_;
     cmd.param1 = heading;
     cmd.param2 = 0;
@@ -68,7 +71,7 @@ void SetYaw(uint8_t relative,double heading){
 
 // Set GPS Position
 void SetGPSPosition(double lat,double lon,double alt){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _SETPOS_;
     cmd.param1 = lat;
     cmd.param2 = lon;
@@ -78,7 +81,7 @@ void SetGPSPosition(double lat,double lon,double alt){
 
 // Set Velocity
 void SetVelocity(double Vn, double Ve, double Vu){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _SETVEL_;
     cmd.param1 = Vn;
     cmd.param2 = Ve;
@@ -88,8 +91,191 @@ void SetVelocity(double Vn, double Ve, double Vu){
 
 // Set Speed
 void SetSpeed(float speed){
-    InitMsg(cmd);
+    InitCmdMsg(cmd);
     cmd.name = _SETSPEED_;
     cmd.param1 = speed;
     SendSBMsg(cmd);
+}
+
+int ServiceFence_GetFenceViolation(double* position,double* velocity){
+    InitGeofenceServiceMsg(msg);
+    memcpy(msg.name,"CheckFenceViolation",19);
+    serializeRealArray(3,position,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceFence_GetWPFeasibility(double* positionA,double* positionB){
+    InitGeofenceServiceMsg(msg);
+    memcpy(msg.name,"CheckDirectPathFeasibility",26);
+    serializeRealArray(3,positionA,msg.buffer);
+    serializeRealArray(3,positionB,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceFence_GetRecoveryPosition(double* position){
+    InitGeofenceServiceMsg(msg);
+    memcpy(msg.name,"CheckDirectPathFeasibility",26);
+    serializeRealArray(3,position,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTraffic_GetTrafficConflict(bool bands,double* position,double* velocity){
+    InitTrafficServiceMsg(msg);
+    memcpy(msg.name,"MonitorTraffic",14);
+    serializeBool(false,bands,msg.buffer);
+    serializeRealArray(3,position,msg.buffer);
+    serializeRealArray(3,velocity,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTraffic_GetSafe2Turn(double* position,double* velocity,double headingA,double headingB){
+    InitTrafficServiceMsg(msg);
+    memcpy(msg.name,"CheckSafeToTurn",15);
+    serializeRealArray(3,position,msg.buffer);
+    serializeRealArray(3,velocity,msg.buffer);
+    serializeReal(false,headingA,msg.buffer);
+    serializeReal(false,headingB,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetWaypoint(char* planID,int index){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"GetWaypoint",11);
+    serializeString(5,planID,msg.buffer);
+    serializeInt(false,index,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetNewPath(char* planID,char* algorithm,double* positionA,double* velocity,double* positionB){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"FindNewPath",11);
+    serializeString(5,planID,msg.buffer);
+    if (!strcmp(algorithm, "GRID")) {
+        serializeString(4,"GRID",msg.buffer);
+    }else if (!strcmp(algorithm, "ASTAR")) {
+        serializeString(5,"ASTAR",msg.buffer);
+    }else if (!strcmp(algorithm, "RRT")) {
+        serializeString(3,"RRT",msg.buffer);
+    }
+    serializeRealArray(3,positionA,msg.buffer);
+    serializeRealArray(3,velocity,msg.buffer);
+    serializeRealArray(3,positionB,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetXTrackDeviation(char* planID,int leg,double* position){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"ComputeCrossTrackDeviation",26);
+    serializeString(5,planID,msg.buffer);
+    serializeInt(false,leg,msg.buffer);
+    serializeRealArray(3,position,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetDistance(double* positionA,double* positionB){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"ComputeDistance",15);
+    serializeRealArray(3,positionA,msg.buffer);
+    serializeRealArray(3,positionB,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetExitPoint(char* planID,double* position,int nextWP){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"GetExitPoint",12);
+    serializeString(5,planID,msg.buffer);
+    serializeRealArray(3,position,msg.buffer);
+    serializeInt(false,nextWP,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetInterceptHeading(char* planID,int nextWP,double* position){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"GetInterceptHeadingToPlan",25);
+    serializeString(5,planID,msg.buffer);
+    serializeInt(false,nextWP,msg.buffer);
+    serializeRealArray(3,position,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+int ServiceTrajectory_GetInterceptManeuver(char* planID,int nextWP,double* position){
+    InitTrajectoryServiceMsg(msg);
+    memcpy(msg.name,"ManeuverToIntercept",19);
+    serializeString(5,planID,msg.buffer);
+    serializeInt(false,nextWP,msg.buffer);
+    serializeRealArray(3,position,msg.buffer);
+    SendSBMsg(msg);
+    return msg.id;
+}
+
+void ServiceFence_DecodeFenceViolation(service_t* msg,bool* output){
+    const char* b  = msg->buffer;
+    b = deSerializeBoolArray(output,b);
+}
+
+void ServiceFence_DecodeWPFeasibility(service_t* msg,bool* output){
+    const char* b = msg->buffer;
+    b = deSerializeBool(false,output,b);
+}
+
+void ServiceFence_DecodeRecoveryPosition(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    b = deSerializeRealArray(output,b);
+}
+
+void ServiceTraffic_DecodeTrafficConflict(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    double resolution[4];
+    b = deSerializeRealArray(resolution,b);
+    *output = resolution[0];
+}
+
+void ServiceTraffic_DecodeSafe2Turn(service_t* msg,bool* output){
+    const char* b = msg->buffer;
+    b = deSerializeBool(false,output,b);
+}
+
+void ServiceTrajectory_DecodeWaypoint(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    b = deSerializeRealArray(output,b);
+}
+
+void ServiceTrajectory_DecodeTotalWaypoints(service_t* msg,int* output){
+    const char* b = msg->buffer;
+    b = deSerializeInt(false,output,b);
+}
+
+void ServiceTrajectory_DecodeNewPath(service_t* msg,int* output){
+    const char* b = msg->buffer;
+    b = deSerializeInt(false,output,b);
+}
+
+void ServiceTrajectory_DecodeXTrackDeviation(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    b = deSerializeReal(false,output,b);
+}
+
+void ServiceTrajectory_DecodeDistance(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    b = deSerializeReal(false,output,b);
+}
+
+void ServiceTrajectory_DecodeExitPoint(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    b = deSerializeRealArray(output,b);
+}
+void ServiceTrajectory_DecodeInterceptHeading(service_t* msg,double* output){
+    const char* b = msg->buffer;
+    b = deSerializeReal(false,output,b);
 }
