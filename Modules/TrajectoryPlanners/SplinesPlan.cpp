deae84e624e5e541c9406b57bdbb9ffa7c10d3eb
swee <swee@umich.edu>
2018-04-06 11:00:09 -0400

    Modified function names

--- Modules/TrajectoryPlanners/SplinesPlan.cpp
+++ Modules/TrajectoryPlanners/SplinesPlan.cpp
@@ -2,7 +2,7 @@
 // Created by research133 on 4/3/18.
 //
 
-int64_t PathPlanner::FindPathSplines(char planID[],double fromPosition[],double toPosition[],double velocity[]){
+int64_t PathPlanner::FindPathBSplines(char planID[],double fromPosition[],double toPosition[],double velocity[]){
 
 
 
