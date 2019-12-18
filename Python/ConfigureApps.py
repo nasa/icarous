1cf53619982be758ad964301ed6626c907c267df
swee warman <sweewarman.balachandran@nasa.gov>
2019-12-18 13:00:53 -0500

    handle should start entry

--- Python/ConfigureApps.py
+++ Python/ConfigureApps.py
@@ -27,14 +27,20 @@ for app in sys.argv[3:]:
 
 for app in sys.argv[3:]:
     appdata = appcfg[app]
-    outfp.write(appdata['OBJ_TYPE']+', ')
-    outfp.write(appdata['APP_PATH']+', ')
-    outfp.write(appdata['ENTRY_PT']+', ')
-    outfp.write(appdata['CFE_NAME']+', ')
-    outfp.write(appdata['PRIORITY']+', ')
-    outfp.write(appdata['STACK_SIZE']+', ')
-    outfp.write(appdata['LOAD_ADDR']+', ')
-    outfp.write(appdata['EXCEPT_ACT']+';\n')
+    try:
+        if(int(appdata['SHOULD_START']) == 1):
+            print("-- Writing config for app: %s"%(app))
+            outfp.write(appdata['OBJ_TYPE']+', ')
+	    outfp.write(appdata['APP_PATH']+', ')
+	    outfp.write(appdata['ENTRY_PT']+', ')
+	    outfp.write(appdata['CFE_NAME']+', ')
+	    outfp.write(appdata['PRIORITY']+', ')
+	    outfp.write(appdata['STACK_SIZE']+', ')
+	    outfp.write(appdata['LOAD_ADDR']+', ')
+	    outfp.write(appdata['EXCEPT_ACT']+';\n')
+    except:
+        print("Error in config: Check configuration for %s"%(app))
+   
 
 
 
