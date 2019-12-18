602bc353f2d24b10c1f9b3a5e136b18484689d18
swee warman <sweewarman.balachandran@nasa.gov>
2019-12-18 16:41:16 -0500

    add missing rotorsim files

---
+++ apps/rotorsim/fsw/platform_inc/rotorsim_msgids.h
@@ -0,0 +1,22 @@
+/**
+ * @file rotorsim_msgids.h
+ * @brief Defines message topics published by the rotorsim application.
+ */
+
+#ifndef ICAROUS_CFS_ROTORSIM_MSGIDS_H
+#define ICAROUS_CFS_ROTORSIM_MSGIDS_H
+
+
+/**
+ * @defgroup ROTORSIM_MESSAGE_TOPICS
+ * @brief Rotorsim message topics
+ * @ingroup ROTORSIM
+ * @ingroup TOPICS
+ * @{
+ */
+
+#define ROTORSIM_PARAMETERS_MID        0x0142    ///< Rotorsim parameters
+
+/**@}*/
+
+#endif //ICAROUS_CFS_GEOFENCE_MSGIDS_H
