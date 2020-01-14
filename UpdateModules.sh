4c8a7746fd3bcfc39d66f1056afd9d1981d99188
Swee Balachandran <sweewarman.balachandran@nasa.gov>
2020-01-14 10:22:40 -0500

    Update modules script

--- UpdateModules.sh
+++ UpdateModules.sh
@@ -1,3 +1,3 @@
-git submodule update cfe --recursive
-git submodule update osal --recursive
-git submodule update psp --recursive
+git submodule update --init --recursive cfe
+git submodule update --init --recursive osal
+git submodule update --init --recursive psp
