9dae87afc819ef1953cafffe6788b7f2a263f9d5
swee warman <sweewarman.balachandran@nasa.gov>
2020-01-30 13:21:56 -0500

    Update Modules script

--- UpdateModules.sh
+++ UpdateModules.sh
@@ -2,3 +2,5 @@
 git submodule update --init --recursive cfe
 git submodule update --init --recursive osal
 git submodule update --init --recursive psp
+git submodule update --init --recursive tools/elf2cfetbl
+git submodule update --init --recursive tools/gen_sch_tbl
