daf87300e88b571e78759bfd5665bda1448ba6d7
Marco Feliu <mfeliuga@rita.ndc.nasa.gov>
2020-04-28 07:36:56 -0400

    [traffic] Create 'band_report_t'/ICAROUS_BAND_REPORT_MID message to aggregate all band messages

--- apps/traffic/fsw/platform_inc/traffic_msgids.h
+++ apps/traffic/fsw/platform_inc/traffic_msgids.h
@@ -17,6 +17,7 @@
 #define ICAROUS_BANDS_SPEED_MID          0x0351 ///< Kinematic speed traffic bands. message type: cfsBands_t
 #define ICAROUS_BANDS_VS_MID             0x0352 ///< Kinematic vertical speed traffic bands. message type: cfsBands_t
 #define ICAROUS_BANDS_ALT_MID            0x0353 ///< Kinematic altitude speed bands. message type: cfsBands_t
+#define ICAROUS_BAND_REPORT_MID          0x0354 ///< Kinematic bands report. message type: band_report_t
 #define TRAFFIC_TURN_CHECK_MID           0x0336 ///< Request to check if a turn is safe. message type: checkTurnSafety_t
 #define TRAFFIC_TURN_CHKECK_RESPONSE_MID 0x0337 ///< Response to turn safety query. message type: checkTurnSafety_t
 #define TRAFFIC_ALERTS_MID               0x0338 ///< Traffic Alerts information
