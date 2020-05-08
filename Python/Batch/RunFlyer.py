[Python] Fix velocity sign conventions

Python/Batch/RunFlyer.py
@@ -2,6 +2,7 @@ from Icarous import *
 from matplotlib import pyplot as plt
 from Animation import AgentAnimation
 
+
 # Set the home position for the simulation
 HomePos = [37.55290000,-122.27250000,0.000000]
 
@@ -12,7 +13,7 @@ tfList = []
 # range,brg,alt,track,speed,climb rate
 # Call this function again to start multiple traffic vehicles
 #StartTraffic(1,HomePos,15000,309,5,50,127,0,tfList)
-StartTraffic(1,HomePos,14000,309,100,50,127,0,tfList)
+StartTraffic(1,HomePos,26000,309,1000,50,127,0,tfList)
 
 # Set uncertainty and smoothing params for traffic
 #for tf in tfList:
@@ -57,9 +58,11 @@ plt.plot(np.array(ic.localPlans[0])[:,1],np.array(ic.localPlans[0])[:,0],'g--')
 plt.scatter(np.array(ic.localPlans[0])[:,1],np.array(ic.localPlans[0])[:,0])
 for tf in tfList:
     plt.plot(np.array(tf.log['pos'])[:,0],np.array(tf.log['pos'])[:,1],'b')
+
+plt.figure(2)
+plt.plot([i for i in range(len(ic.positionLog))],np.array(ic.positionLog)[:,2])
+
 plt.show()
-
-
 anim= AgentAnimation(-15000,-2000, 100,15000,5)
 for pln in ic.localPlans:
     anim.AddPath(np.array(pln),'--')

Python/Batch/RunPySim.py
@@ -51,8 +51,10 @@ plt.figure(1)
 plt.plot(np.array(ic.positionLog)[:,0],np.array(ic.positionLog)[:,1],'r')
 for tf in tfList:
     plt.plot(np.array(tf.log['pos'])[:,0],np.array(tf.log['pos'])[:,1],'b')
-plt.show()
 
+plt.figure(2)
+plt.plot([i for i in range(len(ic.positionLog))],np.array(ic.positionLog)[:,2])
+plt.show()
 
 anim= AgentAnimation(-50,-50, 100,30,30)
 anim.AddPath(np.array(ic.localPlans[0]),'k--')

Python/Batch/ichelper.py
@@ -48,22 +48,22 @@ def gps_offset(lat, lon, east, north):
     distance = math.sqrt(east**2 + north**2)
     return gps_newpos(lat, lon, bearing, distance)
 
-def ConvertVnedToTrkGsVs(ve,vn,vz):
+def ConvertVnedToTrkGsVs(vn,ve,vz):
     angle = 360 + np.arctan2(ve,vn) * 180/np.pi
     trk = np.fmod(angle,360)
     gs = np.sqrt(vn**2 + ve**2)
-    vs = vz
+    vs = -vz
     return (trk,gs,vs)
 
 def ComputeHeading(start,end):
     relvec = [end[0]-start[0],end[1]-start[1],end[2]-start[2]]
-    (trk,gs,vs) = ConvertVnedToTrkGsVs(relvec[0],relvec[1],relvec[2])
+    (trk,gs,vs) = ConvertVnedToTrkGsVs(relvec[1],relvec[0],-relvec[2])
     return trk
 
 def ConvertTrkGsVsToVned(trk,gs,vs):
     vy = gs * np.cos(trk*np.pi/180)
     vx = gs * np.sin(trk*np.pi/180)
-    vz = vs
+    vz = -vs
     return (vy,vx,vz)
 
 def GetInitialConditions():