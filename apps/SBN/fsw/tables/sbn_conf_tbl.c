c7ec890e605a68f84ced06634b348c996915b6ff
Swee <swee.balachandra@nianet.org>
2020-07-01 14:53:33 -0400

    [SBN] Update to new version

--- apps/SBN/fsw/tables/sbn_conf_tbl.c
+++ apps/SBN/fsw/tables/sbn_conf_tbl.c
@@ -11,62 +11,56 @@ SBN_ConfTbl_t SBN_ConfTbl =
             .LibSymbol = "SBN_UDP_Ops",
             .BaseEID = 0x0100
         },
-        { /* [1] */
-            .Name = "TCP",
-            .LibFileName = "/cf/sbn_tcp.so",
-            .LibSymbol = "SBN_TCP_Ops",
-            .BaseEID = 0x0200
-        }
+        //{ /* [1] */
+        //    .Name = "TCP",
+        //    .LibFileName = "/cf/sbn_tcp.so",
+        //    .LibSymbol = "SBN_TCP_Ops",
+        //    .BaseEID = 0x0200
+        //}
     },
-    .ProtocolCnt = 2,
+    .ProtocolCnt = 1,
     .FilterModules =
     {
-        { /* [0] */
-            .Name = "CCSDS Endian",
-            .LibFileName = "/cf/sbn_f_ccsds_end.so",
-            .LibSymbol = "SBN_F_CCSDS_End",
-            .BaseEID = 0x1000
-        },
-        { /* [1] */
-            .Name = "Test",
-            .LibFileName = "/cf/sbn_f_test.so",
-            .LibSymbol = "SBN_F_Test",
-            .BaseEID = 0x1100
-        }
+        //{ /* [0] */
+        //    .Name = "CCSDS Endian",
+        //    .LibFileName = "/cf/sbn_f_ccsds_end.so",
+        //    .LibSymbol = "SBN_F_CCSDS_End",
+        //    .BaseEID = 0x1000
+        //},
+        //{ /* [1] */
+        //    .Name = "Remap",
+        //    .LibFileName = "/cf/sbn_f_remap.so",
+        //    .LibSymbol = "SBN_F_Remap",
+        //    .BaseEID = 0x1000
+        // }
     },
-    .FilterCnt = 2,
+    .FilterCnt = 0,
 
     .Peers = {
         { /* [0] */
             .ProcessorID = 1,
-            .SpacecraftID = 42,
+            .SpacecraftID = 0,
             .NetNum = 0,
             .ProtocolName = "UDP",
-            .Filters = {
-                "CCSDS Endian"
-            },
+            .Filters = { },
             .Address = "127.0.0.1:2234",
             .TaskFlags = SBN_TASK_POLL
         },
         { /* [1] */
             .ProcessorID = 2,
-            .SpacecraftID = 42,
+            .SpacecraftID = 1,
             .NetNum = 0,
             .ProtocolName = "UDP",
-            .Filters = {
-                "CCSDS Endian"
-            },
+            .Filters = { },
             .Address = "127.0.0.1:2235",
             .TaskFlags = SBN_TASK_POLL
         },
         { /* [2] */
             .ProcessorID = 3,
-            .SpacecraftID = 42,
+            .SpacecraftID = 2,
             .NetNum = 0,
             .ProtocolName = "UDP",
-            .Filters = {
-                "CCSDS Endian"
-            },
+            .Filters = { },
             .Address = "127.0.0.1:2236",
             .TaskFlags = SBN_TASK_POLL
         },
