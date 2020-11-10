ac0a65bab6763e0916a900dd470b5a10de4f7efb
Swee Balachandran <sweewarman.balachandran@nasa.gov>
2020-11-10 15:53:12 -0500

    [pycarous] Use exception handling when import spq model

--- Python/pycarous/vehiclesim/__init__.py
+++ Python/pycarous/vehiclesim/__init__.py
@@ -1,8 +1,8 @@
 from vehiclesim.VehicleSimInterface import VehicleSimInterface
 try:
     from vehiclesim.quadsim import QuadSim
+    from vehiclesim.uamsim import UamVtolSim
+    from vehiclesim.spqsim import SixPassengerQuadSim
 except:
     pass
 from vehiclesim.uamsim import UamVtolSim
-from vehiclesim.spqsim import SixPassengerQuadSim
-from vehiclesim import SpqInterface
