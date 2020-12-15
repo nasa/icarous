02ed16d49fa9c60d7f34be8817bf959980a0b8e3
Swee Balachandran <sweewarman.balachandran@nasa.gov>
2020-12-15 14:46:57 -0500

    [pycarous] Add package files to pycarous

---
+++ Python/pycarous/__init__.py
@@ -0,0 +1,4 @@
+# Wrappers for Core Icarous Modules
+from pycarous import vehiclesim 
+from pycarous import Icarous
+from pycarous import SimEnvironment 
--- Python/pycarous/vehiclesim/__init__.py
+++ Python/pycarous/vehiclesim/__init__.py
@@ -1,7 +1,16 @@
 from vehiclesim.VehicleSimInterface import VehicleSimInterface
+availableSimTypes = []
 try:
     from vehiclesim.quadsim import QuadSim
-    from vehiclesim.spqsim import SixPassengerQuadSim
+    availableSimTypes.append('UAS_ROTOR')
 except:
     pass
+
+try:
+    from vehiclesim.spqsim import SixPassengerQuadSim
+    availableSimTypes.append('UAM_SPQ')
+except:
+    pass
+
 from vehiclesim.uamsim import UamVtolSim
+availableSimTypes.append('UAM_VTOL')
