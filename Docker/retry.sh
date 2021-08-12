ebb27c8f77417a51eb454c33095eafbb755e6cb6
Marco Feliu <mfeliuga@rita.ndc.nasa.gov>
2021-08-12 17:01:30 -0400

    [Docker] Add retry.sh hack to repeat git cloning operations

---
+++ Docker/retry.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Retries a command on failure.
+# $1 - the max number of attempts
+# $2... - the command to run
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -r cmd="$@"
+    local -i attempt_num=1
+    until $cmd
+    do
+        if (( attempt_num == max_attempts ))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+            sleep $(( attempt_num++ ))
+        fi
+    done
+}
+# example usage:
+# retry 5 ls -ltr foo
