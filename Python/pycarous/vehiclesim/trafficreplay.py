4959f5eaa6607bd025f39a0dc01394736dffc469
Swee Balachandran <sweewarman.balachandran@nasa.gov>
2021-10-08 14:43:25 -0400

    [pycarous] Add sigma to traffic message

--- Python/pycarous/vehiclesim/trafficreplay.py
+++ Python/pycarous/vehiclesim/trafficreplay.py
@@ -18,6 +18,7 @@ class TrafficReplay(VehicleSimInterface):
             log files using the read_traffic.py tool in SIRIUS
         :param channel: communication channel to replay traffic messages on
         :param delay: number of seconds to wait before starting log replay
+        :param filter: list of callsigns to skip
         """
         self.delay = delay
         self.transmitter = get_transmitter("GroundTruth", "TrafficReplay", channel)
@@ -30,20 +31,40 @@ class TrafficReplay(VehicleSimInterface):
         self.position_updates.sort_index(inplace=True)
         self.homePos = homePos
         self.VNED = False
-
+        self.uncertainty = False
+        
+        self.units = {}
         # Position units
         if 'lat' not in units.index:
-            posunits = list(map(lambda x:From_string(x,1),[units.sy,units.sx,units.sz]))
+            self.units['sx'] = From_string(units.sx,1)
+            self.units['sy'] = From_string(units.sy,1)
+            self.units['sz'] = From_string(units.sz,1)
         else:
-            posunits = [1,1,From_string(units.alt,1)] # Assume degrees is default
+            self.units['lat'] = 1
+            self.units['lon'] = 1
+            self.units['alt'] = From_string(units.alt,1)
 
         # velocity units
         if 'vn' in units.index:
-            velunits = list(map(lambda x:From_string(x,1),[units.vn,units.ve,units.vd]))
+            self.units['vn'] =  From_string(units.vn,1)
+            self.units['ve'] =  From_string(units.ve,1)
+            self.units['vd'] =  From_string(units.vd,1)
             self.VNED = True
         else:
-            velunits = list(map(lambda x:From_string(x,1),[units.vy,units.vx,units.vz]))
-        self.units = posunits+velunits 
+            self.units['vy'] =  From_string(units.vx,1)
+            self.units['vx'] =  From_string(units.vy,1)
+            self.units['vz'] =  From_string(units.vz,1)
+
+        if 's_EW_std' in units.s_EW_std:
+            self.uncertainty = True
+            self.units['s_EW_std'] = From_string(units.s_EW_std,1)
+            self.units['s_NS_std'] = From_string(units.s_NS_std,1)
+            self.units['s_EN_std'] = From_string(units.s_EN_std,1)
+            self.units['s_z_std'] = From_string(units.s_z_std,1)
+            self.units['v_EW_std'] = From_string(units.v_EW_std,1)
+            self.units['v_NS_std'] = From_string(units.v_NS_std,1)
+            self.units['v_EN_std'] = From_string(units.v_EN_std,1)
+            self.units['v_z_std'] = From_string(units.v_z_std,1)
 
         self.filter = filter
 
@@ -74,20 +95,34 @@ class TrafficReplay(VehicleSimInterface):
             if traffic_msg.NAME in self.filter:
                 continue 
             if self.homePos is not None:
-                posLLA = ConvertNED2LLA(self.homePos,[traffic_msg.sy * self.units[0],traffic_msg.sx*self.units[1],-traffic_msg.sz*self.units[2]])
+                posLLA = ConvertNED2LLA(self.homePos,[traffic_msg.sy * self.units['sy'],traffic_msg.sx*self.units['sx'],-traffic_msg.sz*self.units['sz']])
             else:
-                posLLA = [traffic_msg.lat*self.units[0], traffic_msg.lon*self.units[1], traffic_msg.alt*self.units[2]]
+                posLLA = [traffic_msg.lat*self.units['lat'], traffic_msg.lon*self.units['lon'], traffic_msg.alt*self.units['alt']]
 
             if self.VNED:
-                velNED = [traffic_msg.vn*self.units[3], traffic_msg.ve*self.units[4], traffic_msg.vd*self.units[5]]
+                velNED = [traffic_msg.vn*self.units['vn'], traffic_msg.ve*self.units['ve'], traffic_msg.vd*self.units['vd']]
             else:
-                velNED = [traffic_msg.vy*self.units[3], traffic_msg.vx*self.units[4], -traffic_msg.vz*self.units[5]]
+                velNED = [traffic_msg.vy*self.units['vy'], traffic_msg.vx*self.units['vx'], -traffic_msg.vz*self.units['vd']]
+            
+            sigmaP = [0.0,0.0,0.0,0.0,0.0,0.0]
+            sigmaV = [0.0,0.0,0.0,0.0,0.0,0.0]
+            if self.uncertainty:
+                sigmaP[0] = (traffic_msg.s_EW_std*self.units['s_EW_std'])**2
+                sigmaP[1] = (traffic_msg.s_NS_std*self.units['s_NS_std'])**2
+                sigmaP[2] = (traffic_msg.s_z_std*self.units['s_z_std'])**2,
+                sigmaP[3] = (traffic_msg.s_EN_std*self.units['s_EN_std'])**2
+                sigmaV[0] = (traffic_msg.v_EW_std*self.units['v_EW_std'])**2
+                sigmaV[1] = (traffic_msg.v_NS_std*self.units['v_NS_std'])**2
+                sigmaV[2] = (traffic_msg.v_z_std*self.units['v_z_std'])**2,
+                sigmaV[3] = (traffic_msg.v_EN_std*self.units['v_EN_std'])**2
 
             msg_data = {
                 "source": self.transmitter.sensorType,
                 "callsign": "replay_" + traffic_msg.NAME,
                 "pos": posLLA,
                 "vel": velNED,
+                "sigmaP": sigmaP,
+                "sigmaV": sigmaV,
             }
             msg = V2Vdata("INTRUDER", msg_data)
             self.transmitter.transmit(current_time, posLLA, msg)
