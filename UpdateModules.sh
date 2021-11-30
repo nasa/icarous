158d4c7e70c8c6d3d4d2eea3be43b983eda39cf8
Swee Balachandran <sweewarman.balachandran@nasa.gov>
2021-11-30 13:21:02 -0500

    Update submodules

--- UpdateModules.sh
+++ UpdateModules.sh
@@ -4,3 +4,4 @@ git submodule update --init --recursive osal
 git submodule update --init --recursive psp
 git submodule update --init --recursive tools/elf2cfetbl
 git submodule update --init --recursive tools/gen_sch_tbl
+git submodule update --init --recursive Modules/Core/TargetTracker/eigen
